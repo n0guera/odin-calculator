aa46b5c03c957a6ce7a4a162a8bfd5cbebc4ac8d
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-03-13 15:41:52 -0300

    Add logic to display operators and clear button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const display = document.querySelector('#display');
 
-const numbers = document.querySelectorAll('.number');
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', toDisplay);
+const numberButtons = document.querySelectorAll('.number');
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', toDisplay);
+}
+
+const operatorButtons = document.querySelectorAll('.operator');
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', toDisplay);
+}
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clearDisplay);
+
+function clearDisplay() {
+    display.textContent = null;
 }
 
 function toDisplay() {
