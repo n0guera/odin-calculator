c646f9842fee03964bef02d6d6817050f1245035
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-03-14 20:03:53 -0300

    Make the calculator actually functional

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ clearButton.addEventListener('click', clearDisplay);
 
 const equalsButton = document.querySelector('#equals');
 
-const resultOnDisplay = false;
+let resultOnDisplay = false;
 
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', function(e) {
@@ -18,7 +18,7 @@ for (let i = 0; i < numberButtons.length; i++) {
 
         if (resultOnDisplay === false) {
             display.textContent += e.target.textContent;
-        } else if (resultOnDisplay === true && lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+        } else if (resultOnDisplay === true && lastChar === '+' || lastChar === '-' || lastChar === '×' || lastChar === '/') {
             resultOnDisplay = false;
             display.textContent += e.target.textContent;
         } else {
@@ -34,7 +34,7 @@ for (let i = 0; i < operatorButtons.length; i++) {
         let currentString = display.textContent;
         let lastChar = currentString[currentString.length - 1];
 
-        if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+        if (lastChar === '+' || lastChar === '-' || lastChar === '×' || lastChar === '/') {
             let newString = currentString.substring(0, currentString.length - 1) + e.target.textContent;
             display.textContent = newString;
         } else if (currentString.length === 0) {
@@ -44,6 +44,43 @@ for (let i = 0; i < operatorButtons.length; i++) {
         }
     });
 }
+
+equalsButton.addEventListener('click', function() {
+    const displayString = display.textContent;
+    const numbers = displayString.split(/\+|\-|\×|\//g);
+    const operators = displayString.replace(/[0-9]|\./g, '').split('');
+
+    let divide = operators.indexOf('/');
+    while (divide != -1) {
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf('/');
+    }
+
+    let multiply = operators.indexOf('×');
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf('×');
+    }
+
+    let subtract = operators.indexOf('-');
+    while (subtract != -1) {
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf('-');
+    }
+
+    let add = operators.indexOf('+');
+    while (add != -1) {
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf('+');
+    }
+
+    display.textContent = numbers[0];
+    resultOnDisplay = true;
+});
 
 function clearDisplay() {
     display.textContent = null;
